27a5472da4edc9859aa4ffdc9a2e8a6263b305cb
Vitto Lewerissa <rvlewerissa@gmail.com>
2017-08-06 03:13:00 +0700

    add searchbar and filtering

--- src/core-ui/header.js
+++ src/core-ui/header.js
@@ -1,29 +1,93 @@
 // @flow
 
-import React from "react";
-import { Header } from "react-native-elements";
+import React, { Component } from "react";
+import autobind from "class-autobind";
+import { Header, SearchBar, Icon } from "react-native-elements";
 import { WHITE, ORANGE } from "../constants/color";
 
 type Props = {
   text: string,
   leftIcon?: string,
-  onPress?: () => void
+  onPress?: () => void,
+  onChangeText?: (text: string) => void
 };
 
-export default function header(props: Props) {
-  let { leftIcon, text, onPress } = props;
-  let centerIcon = {
-    text,
-    style: { color: WHITE }
-  };
+type State = {
+  showSearchBar: boolean
+};
 
-  return (
-    <Header
-      leftComponent={getIcon(leftIcon, onPress)}
-      centerComponent={centerIcon}
-      outerContainerStyles={{ backgroundColor: ORANGE }}
-    />
-  );
+export default class TopBar extends Component {
+  state: State;
+  props: Props;
+  constructor() {
+    super(...arguments);
+    autobind(this);
+    this.state = {
+      showSearchBar: false
+    };
+  }
+  render() {
+    let { leftIcon, onPress } = this.props;
+    return (
+      <Header
+        leftComponent={getIcon(leftIcon, onPress)}
+        centerComponent={this._getCenterComponent()}
+        rightComponent={this._getRightComponent()}
+        outerContainerStyles={{ backgroundColor: ORANGE }}
+      />
+    );
+  }
+
+  _getRightComponent() {
+    let { showSearchBar } = this.state;
+    let { text } = this.props;
+    if (text !== "News List") return null;
+    return !showSearchBar
+      ? <Icon
+          name="search"
+          color={WHITE}
+          onPress={() => this.setState({ showSearchBar: true })}
+        />
+      : <Icon
+          name="close"
+          color={WHITE}
+          onPress={() => this.setState({ showSearchBar: false })}
+        />;
+  }
+
+  _getCenterComponent() {
+    let { showSearchBar } = this.state;
+    let { text } = this.props;
+    return showSearchBar
+      ? this._renderSearchBar()
+      : {
+          text,
+          style: { color: WHITE }
+        };
+  }
+
+  _renderSearchBar() {
+    return (
+      <SearchBar
+        round
+        lightTheme
+        onChangeText={this._onChangeText}
+        inputStyle={{
+          backgroundColor: WHITE
+        }}
+        containerStyle={{
+          backgroundColor: ORANGE,
+          borderBottomColor: ORANGE,
+          borderTopColor: ORANGE,
+          width: 250
+        }}
+      />
+    );
+  }
+
+  _onChangeText(text: string) {
+    this.props.onChangeText(text);
+  }
 }
 
 function getIcon(icon?: string, onPress?: () => void) {
--- src/features/NewsList/newslist.js
+++ src/features/NewsList/newslist.js
@@ -3,6 +3,7 @@
 import React, { Component } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import { List, ListItem } from "react-native-elements";
+import autobind from "class-autobind";
 
 import Header from "../../core-ui/Header";
 import LoadingIndicator from "../../core-ui/LoadingIndicator";
@@ -22,10 +23,19 @@ type Props = {
   navigateTo: (newsURI: string, newsTitle: string) => void
 };
 
+type State = {
+  searchNews: string
+};
+
 export default class NewsList extends Component {
+  state: State;
   props: Props;
   constructor() {
     super(...arguments);
+    autobind(this);
+    this.state = {
+      searchNews: ""
+    };
   }
 
   render() {
@@ -33,7 +43,12 @@ export default class NewsList extends Component {
     let news = data && data.articles;
     return (
       <ScrollView style={styles.root}>
-        <Header text="News List" leftIcon="arrow-back" onPress={navigateBack} />
+        <Header
+          text="News List"
+          leftIcon="arrow-back"
+          onPress={navigateBack}
+          onChangeText={this._onChangeText}
+        />
         <View style={styles.contentWrapper}>
           <List>
             {news ? this._renderNews(news) : <LoadingIndicator />}
@@ -45,16 +60,23 @@ export default class NewsList extends Component {
 
   _renderNews(articles: Array<Object>) {
     let { navigateTo } = this.props;
-    return articles.map(({ title, description, url, urlToImage }, i) =>
-      <ListItem
-        roundAvatar
-        avatar={{ uri: urlToImage }}
-        key={i}
-        title={title}
-        subtitle={description}
-        onPress={() => navigateTo(url, title)}
-      />
-    );
+    let { searchNews } = this.state;
+    return articles
+      .filter(({ title }) => title.toLowerCase().includes(searchNews))
+      .map(({ title, description, url, urlToImage }, i) =>
+        <ListItem
+          roundAvatar
+          avatar={{ uri: urlToImage }}
+          key={i}
+          title={title}
+          subtitle={description}
+          onPress={() => navigateTo(url, title)}
+        />
+      );
+  }
+
+  _onChangeText(text: string) {
+    this.setState({ searchNews: text });
   }
 }
 

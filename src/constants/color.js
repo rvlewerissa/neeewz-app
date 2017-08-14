1d84296fd979e8ac217df89e6bb9ab7d5ec48066
Vitto Lewerissa <rvlewerissa@gmail.com>
2017-08-14 15:31:41 +0700

    minor color refactoring

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,12 +1,11 @@
 // @flow
 
-export const WHITE = "rgb(250,255,255)";
-export const ZERO_OPACITY = "rgba(0, 0, 0, 0)";
-export const ORANGE = "rgb(255, 161, 19)";
+export const WHITE = "#FAFFFF";
+export const ORANGE = "#FFA113";
 export const JAGGER = "#433B47";
-export const GREY = "rgb(213, 215, 218)";
+export const GREY = "#D5D7DA";
 export const LIGHT_GREY = "#A9A8AB";
 export const LIGHT_GREY_2 = "#F7F7F8";
 export const LIGHT_GREY_3 = "#433B47";
 export const DARK = "#433B47";
-export const LIGHT_WHITE = "rgb(224, 224, 224)";
+export const LIGHT_WHITE = "#E0E0E0";
